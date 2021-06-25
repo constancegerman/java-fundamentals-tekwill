60cdf3cd8d19210cbcd6513af48cc73f88e73c04
Constance <constancegerman@gmail.com>
2021-06-26 01:44:32 +0300

    Added Java Fundamentals tasks

---
+++ src/edu/tekwill/java/App.java
@@ -0,0 +1,12 @@
+package edu.tekwill.java;
+
+/**
+ * @author nsirbu
+ * @since 15.06.2021
+ */
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Bye!");
+  }
+}
